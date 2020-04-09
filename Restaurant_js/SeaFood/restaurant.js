b7a3b3225eba780803b3102c18fbcc36a62f2acd
Chel0cean <chelsea.anna.miller@gmail.com>
2020-04-09 18:25:52 -0400

    update

--- Restaurant_js/SeaFood/restaurant.js
+++ Restaurant_js/SeaFood/restaurant.js
@@ -1,8 +1,6 @@
 //if #Email is =""
-//Return "*Please complete this field"
-function refresh() {
-  document.getElementById("contactForm").reset();
-}
+
+
 function send() {
 var email = String(document.getElementById("reqfield").value);
 if (email == "") {
@@ -14,4 +12,10 @@ document.getElementById("error").style.display = "none";
 document.getElementById("sent").style.display = "block";
 document.getElementById("sent").innerHTML ="<br>*Thank you for your submission! We will be in touch with you within 48 hours.";
 return false;
+}
+
+function refresh() {
+
+  document.getElementById(‘name’).value = ”“;
+  return false;
 }
